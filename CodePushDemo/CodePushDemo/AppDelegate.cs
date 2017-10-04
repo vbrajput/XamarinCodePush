bbdadd5dfb5eda3a8e35f2daad238704fb673671
DeepSinha1 <deep.sinha@niit-tech.com>
2017-10-04 13:14:58 +0530

    * CodePushDemo.userprefs: Fixing in the code
    * AppDelegate.cs:
    * CodePushDemo.exe:
    * CodePushDemo.pdb:
    * CodePushDemo.pdb:
    * CodePushDemo.exe:
    * CodePushDemo.csproj.FileListAbsolute.txt:
    * mtouch.stamp:
    * CodePushDemo.csproj.CoreCompileInputs.cache:
    * mtouch-symbols.list:
    * arguments:
    * MyFirstView.nib:
    * PkgInfo:
    * partial-info.plist:
    * System.dll:
    * Info.plist:
    * System.pdb:
    * asset-manifest.plist:
    * CodePushDemo:
    * entry-points.txt:
    * mscorlib.pdb:
    * mscorlib.dll:
    * System.Xml.pdb:
    * System.Xml.dll:
    * MyFirstView.nib:
    * System.Core.dll:
    * System.Core.pdb:
    * MyFirstView.nib:
    * Xamarin.iOS.dll:
    * _PartialAppManifest.items:
    * CodePushDemo.pdb:
    * CodePushDemo.exe:
    * Mono.Security.pdb:
    * Mono.Security.dll:
    * Xamarin.iOS.dll.mdb:
    * runtime-options.plist:
    * Root.plist:
    * _BundleResourceWithLogicalName.items:
    * _BundleResourceWithLogicalName.items:
    * MonoTouchDebugConfiguration.txt:
    * Contents.json:
    * Contents.json:

--- CodePushDemo/CodePushDemo/AppDelegate.cs
+++ CodePushDemo/CodePushDemo/AppDelegate.cs
@@ -3,24 +3,23 @@ using UIKit;
 
 namespace CodePushDemo
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
     {
         // class-level declarations
 
-        public override UIWindow Window
-        {
-            get;
-            set;
-        }
 
+
+        public   UIWindow window;
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
             // Override point for customization after application launch.
             // If not required for your application you can safely delete this method
 
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            window.RootViewController = new UINavigationController(new MyFirstView());
+            window.MakeKeyAndVisible();
             return true;
         }
 
